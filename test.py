094f29d581ffec22ab7b2505cb115686dfd1d97e
hahakek <157785106+hahakek@users.noreply.github.com>
2024-02-21 20:32:46 +0800

    distance calculation

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
-import time 
-from robotmaster import robot
-from robomaster import vision
+import time
+import math
+from robomaster import robot
 
 class MarkerInfo:
-
     def __init__(self, x, y, w, h, info):
         self._x = x
         self._y = y
@@ -13,22 +12,26 @@ class MarkerInfo:
 
 markers = []
 
+def calculate_distance(w):
+    return 2.8 / math.tan(w * math.pi / 3)
 
 def on_detect_marker(marker_info):
-    number = len(marker_info)
-    markers.clear()
-    for i in range(0, number):
-        x, y, w, h, info = marker_info[i]
-        markers.append(MarkerInfo(x, y, w, h, info))
-        print("marker:{0} x:{1}, y:{2}, w:{3}, h:{4}".format(info, x, y, w, h))
+    global markers
+    print(marker_info)
+    if len(marker_info) > 0:
+        print("Distance: {0}".format(calculate_distance(marker_info[0][2])))
+    markers = marker_info
 
-
+       #add global markers list of marker object
 if __name__ == '__main__':
     ep_robot = robot.Robot()
-    ep_robot.initialize(conn_type="sta")
+    ep_robot.initialize(conn_type="ap")
 
     ep_vision = ep_robot.vision
     ep_camera = ep_robot.camera
 
-    ep_camera.start_video_stream(display=False)
-    result = ep_vision.sub_detect_info(name="marker", callback=on_detect_marker)
+    ep_camera.start_video_stream(display=True)
+    ep_vision.sub_detect_info(name="marker", callback=on_detect_marker)
+
+    input("HI")
+    ep_robot.close()
