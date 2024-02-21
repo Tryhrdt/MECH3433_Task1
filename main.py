a2ff4bdf1badd7352c40357e9818b277fd727502
Tryhrdt <99841148+Tryhrdt@users.noreply.github.com>
2024-02-21 13:42:09 +0800

    vision test

---
+++ main.py
@@ -0,0 +1,60 @@
+# main code for Task 4
+
+import cv2
+import numpy as np
+import robomaster
+from robomaster import robot
+
+current_x, current_y, current_z = (0,0,0)
+
+
+# handler function for EP positional data
+def positional_data_handler(sub_info):
+    current_x, current_y, current_z = sub_info
+
+    current_x *= -1
+    current_y *= -1
+    current_z *= -1
+
+    current_x = round(current_x, 3)
+    current_y = round(current_y, 3)
+    current_z = round(current_z, 3)
+    print("{0}, {1}, {2}".format(current_x, current_y, current_z))
+
+# handler fuction for EP vision data (bowtie)
+# format : [x-coord, y-coord, width of marker, height of marker, marker name]
+# coord system = (0-1)
+# dimension of marker is in coordinates
+# Example: [[0.4375, 0.46806, 0.38438, 0.71389, '2']]
+def vision_info_handler(sub_info):
+    print(sub_info)
+
+# Define robot object
+if __name__ == "__main__":
+    #   Using Wi-Fi Direct method to connect
+    ep_robot = robot.Robot()
+    ep_robot.initialize(conn_type="ap")
+
+    # initialize objects:
+    ep_chassis  = ep_robot.chassis
+    ep_vision   = ep_robot.vision
+    ep_ir       = ep_robot.sensor
+
+
+
+    # initialize coordinate system
+    # x = x-coord, y = y-coord, z = rotation in deg
+    ep_chassis.sub_position(cs=0, freq=10, callback=positional_data_handler)
+
+    # initialize vision system
+    ep_vision.sub_detect_info(name="marker", color="red", callback=vision_info_handler)
+
+    # ep_chassis.move(x=0.6, y=-0.6, z=0, xy_speed=1).wait_for_completed()
+
+# TODO: get marker coordinate in frame here
+
+
+
+
+    input("Press any button to quit:")
+    ep_robot.close()
--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import time
 import robomaster
 from robomaster import robot
-
+global dist1, dist2, dist3, dist4
 
 def arm_pos_handler(sub_info):
     pos_x, pos_y = sub_info
     print("Current pos: {0}, {1}\n".format(pos_x, pos_y))
 
+def dist_sensor_handler(sub_info):
+    dist1, dist2, dist3, dist4 = sub_info
+    print("IR prox distance: {0}".format(dist1))
 
 if __name__ == '__main__':
     # 如果本地IP 自动获取不正确，手动指定本地IP地址
@@ -17,11 +20,17 @@ if __name__ == '__main__':
 
     # main code:
     ep_arm = ep_robot.robotic_arm
-    ep_arm.sub_position(5, callback=arm_pos_handler)
-    time.sleep(1)
-    ep_arm.recenter().wait_for_completed()
-    ep_arm.move(x=200, y=-100).wait_for_completed()
+    ep_sensor = ep_robot.sensor
+    ep_sensor.sub_distance(freq=5, callback=dist_sensor_handler)
+    # ep_arm.sub_position(freq=5, callback=arm_pos_handler)
+    ep_arm.recenter().wait_for_completed(timeout=10)
+    # ep_arm.unsub_position()
+    if dist1 == 0:
+        ep_arm.gripper.close().wait_for_completed()
+        ep_arm.move(y=100).wait_for_completed()
+    
+    ep_sensor.unsub_distance()
 
-    ep_arm.unsub_position()
+
 
     ep_robot.close()
