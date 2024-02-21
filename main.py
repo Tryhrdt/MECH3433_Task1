0ea02f67804a8d4d838e4e11395615e4c8b25fcd
Tryhrdt <99841148+Tryhrdt@users.noreply.github.com>
2024-02-21 19:16:40 +0800

    motion layout planning done

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import robomaster
 from robomaster import robot
 
 current_x, current_y, current_z = (0,0,0)
-
+detect = False
 
 # handler function for EP positional data
 def positional_data_handler(sub_info):
+    global current_x, current_y, current_z
     current_x, current_y, current_z = sub_info
 
     current_x *= -1
@@ -29,8 +30,13 @@ def positional_data_handler(sub_info):
 def vision_info_handler(sub_info):
     print(sub_info)
 
-def distance_handler(sub_info):
-    print(sub_info)
+def ir_handler(sub_info):
+    global detect
+    if sub_info[0] < 30:
+        detect = True
+    else
+        detect = False
+    print(detect)
 
 # Define robot object
 if __name__ == "__main__":
@@ -59,11 +65,28 @@ if __name__ == "__main__":
     ep_camera.start_video_stream(display=True)
 
     # enable IR
-    ep_ir.sub_distance(freq=20, callback=distance_handler)
+    ep_ir.sub_distance(freq=20, callback=ir_handler)
 
-
-# TODO: get marker coordinate in frame here
-
+    '''TODO
+    Find closest vision marker,
+    if vision marker is to the left, move left
+    if vision marker is to the right, move right
+    implement PID?
+    check align
+    if align, get distance to vision marker
+    calculate speed to move towards vision maker safely
+    while moving check if speed is ok with distance data
+    slow down when ir detect
+    close gripper, move back to original position and move horizontally to the next marker given by alex's code
+    calculate distance, move according to distance and initial correction data?
+    stop
+    open gripper
+    move back
+    then go to next vision marker
+    repeat
+    move back to 0,0
+    end
+    '''
 
 
 
