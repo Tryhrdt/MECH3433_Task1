0555189c48cbc80f112af34ba61707bc5e860cd1
aw202 <159109270+aw21alex@users.noreply.github.com>
2024-02-21 16:42:48 +0800

    work in prgress

--- sequence.py
+++ sequence.py
@@ -1,19 +1,46 @@
-# Using recursive function to solve Hanoi Tower.
-def HanoiTower(disk, source, destination, intermediate):
-    if (source != destination) and (source != intermediate) and (destination != intermediate):
-        if disk == 1:
-            print("Move Disk 1 from source", source, "to destination", destination+'.')
-            return
-        HanoiTower(disk - 1, source, intermediate, destination)     # Move disk from source to intermediate to start the recursive sub-system
-        print("Move Disk", disk, "from source", source, "to destination", destination+'.')
-        HanoiTower(disk - 1, intermediate, destination, source)     # Move disk from intermediate to destination to complete the recursive sub-system
+# Input:    Block number, Rods coordinates
+# (can block coordinates merge with rod coordinates? need third coordinates?)
+# Output:   Motion to move block
 
+# Using recursive function to solve Hanoi Tower (By Wong Alex Yu Hin)
+def HanoiTower(block, source, destination, intermediate):
+    if (source != destination) and (source != intermediate) and (destination != intermediate):
+        if block == 1:
+            print("Move the robot from current position to", source+'.')
+            #move(current, source) #move from current place to source
+
+            print("Grab Block 1 at", source + '.')
+            #grab(block1) #identify block1 and grab block1
+
+            print("Move the robot from", source, 'to', destination+'.')
+            #move(source, destination) #move from source to destination
+
+            print("Release Block 1 at", destination + '.')
+            #release(block1) #release block1 above other existing blocks
+            return
+        HanoiTower(block - 1, source, intermediate, destination)     # Move block from source to intermediate to start the recursive sub-system
+
+        print("Move the robot from current position to", source + '.')
+        # move(current, source) #move from current place to source
+
+        print("Grab", block, "at", source + '.')
+        # grab(block) #identify block1 and grab block1
+
+        print("Move the robot from", source, 'to', destination + '.')
+        # move(source, destination) #move from source to destination
+
+        print("Release", block, "at", destination + '.')
+        # release(block) #release block1 above other existing blocks
+
+        HanoiTower(block - 1, intermediate, destination, source)     # Move block from intermediate to destination to complete the recursive sub-system
 
 # Driver code
-disk = int(input("Number of disk: "))
-if disk > 0: HanoiTower(disk, 'A', 'C', 'B')    # A, B, C are the name of rods
-print('Stopping the program...')
+def sequence(block, source, destination, intermediate):
+    if block > 0: HanoiTower(block, source, destination, intermediate)    # A, B, C are the name of rods
+    return
 
-#Reference:
-#https://www.geeksforgeeks.org/python-program-for-tower-of-hanoi/
-#https://www.geeksforgeeks.org/c-program-for-tower-of-hanoi/
+#import block coordinates and map into block1,2,3
+#block = [block1, block2, block3]
+block = int(3)
+source, destination, intermediate = 'A', 'C', 'B'
+sequence(block, source, destination, intermediate)
