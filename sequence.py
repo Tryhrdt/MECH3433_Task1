143211dc570e1c1279a03f9e371dd5e8085c1635
aw21alex <159109270+aw21alex@users.noreply.github.com>
2024-02-23 01:12:00 +0800

    Update sequence.py

--- sequence.py
+++ sequence.py
@@ -1,42 +1,36 @@
-# Input:    Block number, Rods coordinates (can block coordinates merge with rod coordinates? need third coordinates?)
-# Output:   Motion to move block
+# Sequencing Codes  (By Wong Alex Yu Hin)
+# Input:    Number of blocks, Rods/Block coordinates
+# Output:   Motion to move blocks between 'rods'
 
-#import robomaster
-#from robomaster import robot
+height = [ 0, 1, 2, 3 ]  # Should be modified to actual height of stacked blocks, the '0' is to mitigate base-0 counting
+block = ['Block_1', 'Block_2', 'Block_3'] # Block List to contain all blocks (name)
+num = int(3) # Total number of blocks
+source, destination, intermediate = ['0°', height[3]], ['180°',height[1]], ['90°',height[1]]   # Cyclindrical coordinates of three 'rods'
 
-height = [ 0, 1, 2, 3 ]
-Block_1, Block_2, Block_3, Block_4 = ['1x','1y','1z'], ['2x','2y','2z'], ['3x','3y','3z'], ['4x','4y','4z']
-# Block coordinates (import block coordinates and map into block1,2,3?)
-block = [Block_1, Block_2, Block_3, Block_4] # Block List to contain all block coordinates
-num = int(3-1) # The 1 is to change the index to base-0
-source, destination, intermediate = ['Ax','Ay',height[3]], ['Cx','Cy',height[1]], ['Bx','By',height[1]]   # A,B,C should be changed to rod coordinates
-
-# Operation function to contain all actions of moving the block from source to destination/ intermediate pos (By Wong Alex Yu Hin)
+# Operation function to contain all actions of moving the block from source to destination/intermediate 'rod' (By Wong Alex Yu Hin)
 def operation(blockname, source, destination, intermediate):
     if (source != destination) and (source != intermediate) and (destination != intermediate):
-        print("Move the robot from current position to", source)
-        # move(current, source) #move from current place to source
+        #print("Move the robot from current position to", source)
+        moveto(source) #move the robot to source position
 
-        print("Grab", blockname, "at", source[0:1], "with height", source[2])
-        # grab(block) #identify block and grab block
+        #print("Grab", blockname, "at", source[0], "with height", source[1])
+        grab(block) #identify and grab the top block in block stack at source 'rod'
 
-        print("Move the robot from", source, 'to', destination)
-        # move(source, destination) #move from source to destination
+        #print("Move the robot from", source, 'to', destination)
+        moveto(destination) #move the robot with grabbed block from source to destination
 
-        print("Release", blockname, "at", destination[0:1], "with height", destination[2])
-        # release(block) #release block on top (above other existing blocks)
+        #print("Release", blockname, "at", destination[0], "with height", destination[1])
+        release(block) #release block on top of block stack at destination 'rod'
 
-        #print('.')
-        #source[2], destination[2] = height[], height[]
         return
 
-# Using recursive function to solve Hanoi Tower (By Wong Alex Yu Hin)
+# Using recursive function to solve Hanoi Tower with divide and conquer strategy (By Wong Alex Yu Hin)
 def HanoiTower(num, source, destination, intermediate):
-    if num == 0:
+    if num == 1:
         operation(block[0], source, destination, intermediate)
         return
     HanoiTower(num - 1, source, intermediate, destination)     # Move block from source to intermediate to start the recursive sub-system
-    operation(block[num], source, destination, intermediate)
+    operation(block[num - 1], source, destination, intermediate)
     HanoiTower(num - 1, intermediate, destination, source)     # Move block from intermediate to destination to complete the recursive sub-system
 
 # Driver code   (by Wong Alex Yu Hin)
